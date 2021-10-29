6b7b2f3d8bd3312e4297912819dbf2e59ad35de9
saddamHussa1n <tuleubay.safiullin@mail.ru>
2021-10-29 23:53:39 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,126 @@
+import os
+import sys
+from Crypto.Cipher import PKCS1_OAEP
+from PyQt5 import QtWidgets
+from Crypto.PublicKey import RSA
+import socket
+import kbrs
+import window_sign_in
+import window_create_acc
+import fa
+
+key = RSA.generate(2048)
+client_private_key = key
+client_public_key = key.publickey().exportKey()
+
+s = socket.socket()
+s.connect(('127.0.0.1', 9090))
+
+s.sendall(client_public_key)
+enc_block_key = s.recv(4096)
+s.close()
+
+cipher_rsa = PKCS1_OAEP.new(client_private_key)
+block_key = cipher_rsa.decrypt(enc_block_key)
+
+
+class FirstClass(QtWidgets.QMainWindow, window_sign_in.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(FirstClass, self).__init__(parent)
+        self.setupUi(self)
+        self.window_create_acc = None
+        self.fa_window = None
+        self.main_window = None
+        self.setFixedSize(self.size())
+
+        self.pushButton.clicked.connect(self.sign_in)
+
+    def sign_in(self):
+        login = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+        s = socket.socket()
+        s.connect(('127.0.0.1', 9090))
+        s.send(bytes(login, 'utf-8'))
+        s.close()
+        s = socket.socket()
+        s.connect(('127.0.0.1', 9090))
+        s.send(bytes(password, 'utf-8'))
+        s.close()
+        s = socket.socket()
+        s.connect(('127.0.0.1', 9090))
+        ans = s.recv(20)
+        s.close()
+        if ans == b'no':
+            self.show_window_create_acc()
+        else:
+            self.show_fa_window()
+            self.close()
+
+    def show_window_create_acc(self):
+        self.window_create_acc = SecondClass()
+        self.window_create_acc.show()
+
+    def show_fa_window(self):
+        self.fa_window = FourthClass()
+        self.fa_window.show()
+
+
+class SecondClass(QtWidgets.QMainWindow, window_create_acc.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setFixedSize(self.size())
+
+        self.pushButton.clicked.connect(self.add_note)
+
+    def add_note(self):
+        new_login = self.lineEdit.text()
+        new_password = self.lineEdit_2.text()
+        s = socket.socket()
+        s.connect(('127.0.0.1', 9090))
+        s.send(bytes(new_login, 'utf-8'))
+        s.close()
+        s = socket.socket()
+        s.connect(('127.0.0.1', 9090))
+        s.send(bytes(new_password, 'utf-8'))
+        s.close()
+        self.close()
+
+
+class ThirdClass(QtWidgets.QMainWindow, kbrs.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.setFixedSize(self.size())
+
+
+class FourthClass(QtWidgets.QMainWindow, fa.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setFixedSize(self.size())
+        self.main_window = None
+        self.pushButton.clicked.connect(self.verify)
+
+    def verify(self):
+        s = socket.socket()
+        s.connect(('127.0.0.1', 9090))
+        s.send(bytes(self.lineEdit.text(), 'utf-8'))
+        if s.recv(100) == b'ok':
+            self.close()
+            self.main_window = ThirdClass()
+            self.main_window.show()
+        else:
+            self.close()
+        s.close()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = FirstClass()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,151 @@
+import sqlite3
+from string import Template
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+import socket
+import hashlib
+import random
+
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+c.execute(
+    '''CREATE TABLE IF NOT EXISTS account (username TEXT, password TEXT, info TEXT)''')
+c.execute('''SELECT * FROM account''')
+
+block_key = get_random_bytes(16)
+key = RSA.generate(2048)
+server_private_key = key.exportKey()
+server_public_key = key.publickey().exportKey()
+
+sock = socket.socket()
+sock.bind(('127.0.0.1', 9090))
+sock.listen(5)
+conn = sock.accept()[0]
+
+client_public_key = conn.recv(4096)
+cipher_rsa = PKCS1_OAEP.new(RSA.import_key(client_public_key))
+enc_session_key = cipher_rsa.encrypt(block_key)
+conn.send(enc_session_key)
+conn.close()
+
+conn = sock.accept()[0]
+login = conn.recv(1024)
+conn.close()
+conn = sock.accept()[0]
+password = conn.recv(1024)
+conn.close()
+
+hash_login = hashlib.md5(login).hexdigest()
+hash_password = hashlib.md5(password).hexdigest()
+
+c.execute('''SELECT * FROM account WHERE username LIKE ? AND password LIKE ?''',
+          ('%' + hash_login + '%', '%' + hash_password + '%'))
+login_check = c.fetchall()
+conn = sock.accept()[0]
+if not login_check:
+    conn.send(b'no')
+    conn.close()
+    conn = sock.accept()[0]
+    new_login = hashlib.md5(conn.recv(1024)).hexdigest()
+    conn.close()
+    conn = sock.accept()[0]
+    new_password = hashlib.md5(conn.recv(1024)).hexdigest()
+    conn.close()
+    c.execute('''INSERT INTO account (username,password) VALUES (?,?)''', (new_login, new_password))
+    c.execute('''SELECT * FROM account''')
+else:
+    conn.send(b'yes')
+    conn.close()
+    fa_key = str(random.randint(1000, 9999))
+    message_template = Template(fa_key)
+
+    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    s.ehlo()
+    s.starttls()
+
+    s.login('tuleubay.safiullin@gmail.com', 'bfujxwbltvzcngkl')
+
+    names = ['Tuleubay']
+    emails = ['tuleubay.safiullin@mail.ru']
+
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()
+        message = message_template.substitute(PERSON_NAME=name.title())
+        msg['From'] = 'tuleubay.safiullin@mail.ru'
+        msg['To'] = email
+        msg['Subject'] = "This is TEST"
+        msg.attach(MIMEText(message, 'plain'))
+        s.send_message(msg)
+        del msg
+
+    conn = sock.accept()[0]
+    if conn.recv(200) == bytes(fa_key, 'utf-8'):
+        conn.send(b'ok')
+    else:
+        conn.send(b'nope')
+    conn.close()
+
+conn = sock.accept()[0]
+login = conn.recv(1024)
+conn.close()
+conn = sock.accept()[0]
+password = conn.recv(1024)
+conn.close()
+
+hash_login = hashlib.md5(login).hexdigest()
+hash_password = hashlib.md5(password).hexdigest()
+
+c.execute('''SELECT * FROM account WHERE username LIKE ? AND password LIKE ?''',
+          ('%' + hash_login + '%', '%' + hash_password + '%'))
+login_check = c.fetchall()
+conn = sock.accept()[0]
+
+
+if not login_check:
+    conn.send(b'no')
+    conn.close()
+    conn = sock.accept()[0]
+    new_login = hashlib.md5(conn.recv(1024)).hexdigest()
+    conn.close()
+    conn = sock.accept()[0]
+    new_password = hashlib.md5(conn.recv(1024)).hexdigest()
+    conn.close()
+    c.execute('''INSERT INTO account (username,password) VALUES (?,?)''', (new_login, new_password))
+    c.execute('''SELECT * FROM account''')
+else:
+    conn.send(b'yes')
+    conn.close()
+    fa_key = str(random.randint(1000, 9999))
+    message_template = Template(fa_key)
+
+    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    s.ehlo()
+    s.starttls()
+
+    s.login('tuleubay.safiullin@gmail.com', 'bfujxwbltvzcngkl')
+
+    names = ['Tuleubay']
+    emails = ['yzsedu@mailto.plus']
+
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()
+        message = message_template.substitute(PERSON_NAME=name.title())
+        msg['From'] = 'tuleubay.safiullin@mail.ru'
+        msg['To'] = email
+        msg['Subject'] = "This is TEST"
+        msg.attach(MIMEText(message, 'plain'))
+        s.send_message(msg)
+        del msg
+
+    conn = sock.accept()[0]
+    if conn.recv(200) == bytes(fa_key, 'utf-8'):
+        conn.send(b'ok')
+    else:
+        conn.send(b'nope')
+    conn.close()
+
+sock.close()
