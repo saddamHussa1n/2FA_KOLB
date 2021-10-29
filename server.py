4602f261b19ce2a6c105c484fc8d6d3806bdd2e8
saddamHussa1n <tuleubay.safiullin@mail.ru>
2021-10-30 00:55:05 +0300

    Лёня лох, без обид

--- server.py
+++ server.py
@@ -32,120 +32,64 @@ enc_session_key = cipher_rsa.encrypt(block_key)
 conn.send(enc_session_key)
 conn.close()
 
-conn = sock.accept()[0]
-login = conn.recv(1024)
-conn.close()
-conn = sock.accept()[0]
-password = conn.recv(1024)
-conn.close()
-
-hash_login = hashlib.md5(login).hexdigest()
-hash_password = hashlib.md5(password).hexdigest()
-
-c.execute('''SELECT * FROM account WHERE username LIKE ? AND password LIKE ?''',
-          ('%' + hash_login + '%', '%' + hash_password + '%'))
-login_check = c.fetchall()
-conn = sock.accept()[0]
-if not login_check:
-    conn.send(b'no')
+z = 1
+while (z != 0):
+    conn = sock.accept()[0]
+    login = conn.recv(1024)
     conn.close()
     conn = sock.accept()[0]
-    new_login = hashlib.md5(conn.recv(1024)).hexdigest()
+    password = conn.recv(1024)
     conn.close()
+
+    hash_login = hashlib.md5(login).hexdigest()
+    hash_password = hashlib.md5(password).hexdigest()
+
+    c.execute('''SELECT * FROM account WHERE username LIKE ? AND password LIKE ?''',
+              ('%' + hash_login + '%', '%' + hash_password + '%'))
+    login_check = c.fetchall()
     conn = sock.accept()[0]
-    new_password = hashlib.md5(conn.recv(1024)).hexdigest()
-    conn.close()
-    c.execute('''INSERT INTO account (username,password) VALUES (?,?)''', (new_login, new_password))
-    c.execute('''SELECT * FROM account''')
-else:
-    conn.send(b'yes')
-    conn.close()
-    fa_key = str(random.randint(1000, 9999))
-    message_template = Template(fa_key)
-
-    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
-    s.ehlo()
-    s.starttls()
-
-    s.login('tuleubay.safiullin@gmail.com', 'bfujxwbltvzcngkl')
-
-    names = ['Tuleubay']
-    emails = ['tuleubay.safiullin@mail.ru']
-
-    for name, email in zip(names, emails):
-        msg = MIMEMultipart()
-        message = message_template.substitute(PERSON_NAME=name.title())
-        msg['From'] = 'tuleubay.safiullin@mail.ru'
-        msg['To'] = email
-        msg['Subject'] = "This is TEST"
-        msg.attach(MIMEText(message, 'plain'))
-        s.send_message(msg)
-        del msg
-
-    conn = sock.accept()[0]
-    if conn.recv(200) == bytes(fa_key, 'utf-8'):
-        conn.send(b'ok')
+    if not login_check:
+        conn.send(b'no')
+        conn.close()
+        conn = sock.accept()[0]
+        new_login = hashlib.md5(conn.recv(1024)).hexdigest()
+        conn.close()
+        conn = sock.accept()[0]
+        new_password = hashlib.md5(conn.recv(1024)).hexdigest()
+        conn.close()
+        c.execute('''INSERT INTO account (username,password) VALUES (?,?)''', (new_login, new_password))
+        c.execute('''SELECT * FROM account''')
     else:
-        conn.send(b'nope')
-    conn.close()
+        z -= 1
+        conn.send(b'yes')
+        conn.close()
+        fa_key = str(random.randint(1000, 9999))
+        message_template = Template(fa_key)
 
-conn = sock.accept()[0]
-login = conn.recv(1024)
-conn.close()
-conn = sock.accept()[0]
-password = conn.recv(1024)
-conn.close()
+        s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+        s.ehlo()
+        s.starttls()
 
-hash_login = hashlib.md5(login).hexdigest()
-hash_password = hashlib.md5(password).hexdigest()
+        s.login('tuleubay.safiullin@gmail.com', 'bfujxwbltvzcngkl')
 
-c.execute('''SELECT * FROM account WHERE username LIKE ? AND password LIKE ?''',
-          ('%' + hash_login + '%', '%' + hash_password + '%'))
-login_check = c.fetchall()
-conn = sock.accept()[0]
+        names = ['Tuleubay']
+        emails = ['tuleubay.safiullin@mail.ru']
 
+        for name, email in zip(names, emails):
+            msg = MIMEMultipart()
+            message = message_template.substitute(PERSON_NAME=name.title())
+            msg['From'] = 'tuleubay.safiullin@mail.ru'
+            msg['To'] = email
+            msg['Subject'] = "This is TEST"
+            msg.attach(MIMEText(message, 'plain'))
+            s.send_message(msg)
+            del msg
 
-if not login_check:
-    conn.send(b'no')
-    conn.close()
-    conn = sock.accept()[0]
-    new_login = hashlib.md5(conn.recv(1024)).hexdigest()
-    conn.close()
-    conn = sock.accept()[0]
-    new_password = hashlib.md5(conn.recv(1024)).hexdigest()
-    conn.close()
-    c.execute('''INSERT INTO account (username,password) VALUES (?,?)''', (new_login, new_password))
-    c.execute('''SELECT * FROM account''')
-else:
-    conn.send(b'yes')
-    conn.close()
-    fa_key = str(random.randint(1000, 9999))
-    message_template = Template(fa_key)
-
-    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
-    s.ehlo()
-    s.starttls()
-
-    s.login('tuleubay.safiullin@gmail.com', 'bfujxwbltvzcngkl')
-
-    names = ['Tuleubay']
-    emails = ['yzsedu@mailto.plus']
-
-    for name, email in zip(names, emails):
-        msg = MIMEMultipart()
-        message = message_template.substitute(PERSON_NAME=name.title())
-        msg['From'] = 'tuleubay.safiullin@mail.ru'
-        msg['To'] = email
-        msg['Subject'] = "This is TEST"
-        msg.attach(MIMEText(message, 'plain'))
-        s.send_message(msg)
-        del msg
-
-    conn = sock.accept()[0]
-    if conn.recv(200) == bytes(fa_key, 'utf-8'):
-        conn.send(b'ok')
-    else:
-        conn.send(b'nope')
-    conn.close()
+        conn = sock.accept()[0]
+        if conn.recv(200) == bytes(fa_key, 'utf-8'):
+            conn.send(b'ok')
+        else:
+            conn.send(b'nope')
+        conn.close()
 
 sock.close()
